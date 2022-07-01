a4cdd06fc13c3ff6f853bb0b96c7bc71be770170
mwitamarwa <106369843+mwitamarwa@users.noreply.github.com>
2022-07-01 11:36:58 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+/*
+simple calculator
+created by MWITA MARWA  DAVID
+on 	July,1
+c89 compiler
+MIT license
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1,num2,sum,diff,product;
+    float quotient;
+    printf("\tsimple calculator!\n");
+    printf["enter two numbers: "];
+    scanf["%d%d",&num1,num2];
+
+    // computations
+    sum=num1 + num2;
+    diff=num1 - num2;
+    product =num1 *num2;
+    quotient= [float]num1/num2;
+
+    //output
+    printf["%d+%d=&d\n",num2,num2,sum];
+    printf["%d-%d=%d\n",num1,num2,diff];
+    printf["%d*%d=d\n",num1,num2,product];
+    printf["%d/%d=%f\n"num1,num2,quotient];
+
+
+
+    return 0;
+}
